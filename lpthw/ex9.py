76bc631bb649e34791919cbd13090f073fed4ae2
yaraleo <yaraleo@mail.ru>
2018-05-17 14:19:57 +0300

    commit today 17/05/2018 tasks 1

---
+++ lpthw/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay"
+
+print "Here are the wdays: ", days
+print "Here the month", months
+
+print """
+asdfad asdfasdf asdfasdf
+asdf asdfasdfasdfsadfasdfasasda
+asdfasd asdfasdfasdfsadfasdfasasda
+asdfsd asdfasdfasdfsadfasdfasasda
+"""
